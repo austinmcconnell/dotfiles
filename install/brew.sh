7c1ba636390e64adcdf10dbe5e5112b74ba776a4
Austin McConnell <austin.s.mcconnell@gmail.com>
2018-11-14 19:23:49 -0600

    Add install of pyenv-implicit plugin for pyenv

--- install/brew.sh
+++ install/brew.sh
@@ -44,6 +44,8 @@ apps=(
 
 brew install "${apps[@]}"
 
+eval "git clone git://github.com/concordusapps/pyenv-implict.git ~/.pyenv/plugins/pyenv-implict"
+
 export DOTFILES_BREW_PREFIX_COREUTILS=`brew --prefix coreutils`
 set-config "DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_CACHE"
 
