268b28458f9f535d6fb3837e91a95a778936e555
Austin McConnell <austin.s.mcconnell@gmail.com>
2018-11-14 18:39:09 -0600

    Move git configuration into own shell script

--- install.sh
+++ install.sh
@@ -16,10 +16,9 @@ fi
 
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
 # Package managers & packages
+. "$DOTFILES_DIR/install/git.sh"
 . "$DOTFILES_DIR/install/brew.sh"
 . "$DOTFILES_DIR/install/zsh.sh"
 . "$DOTFILES_DIR/install/brew-cask.sh"
---
+++ install/git.sh
@@ -0,0 +1,6 @@
+
+mkdir ~/.git-templates
+
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/git/hooks" ~/.git-templates
