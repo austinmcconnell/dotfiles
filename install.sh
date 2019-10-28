251f3c6c4f5339c856e458d29a014fa6e3f0abd4
Austin McConnell <austin.s.mcconnell@gmail.com>
2019-10-28 13:44:33 -0500

    Add python.sh install script to main install.sh script

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ fi
 . "$DOTFILES_DIR/install/git.sh"
 . "$DOTFILES_DIR/install/brew.sh"
 . "$DOTFILES_DIR/install/brew-cask.sh"
+. "$DOTFILES_DIR/install/python.sh"
 . "$DOTFILES_DIR/install/nvm.sh"
 . "$DOTFILES_DIR/install/vim.sh"
 
