d747052ce741e797232a08ceefbab4dffb4aade4
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-10-15 10:13:54 -0500

    Remove unused macos apps from install list

--- macos/apps.sh
+++ macos/apps.sh
@@ -13,7 +13,6 @@ fi
 
 # Install applications
 brew install --cask atom
-brew install --cask authy
 brew install --cask betterzip
 brew install --cask docker
 brew install --cask dropbox
@@ -28,15 +27,11 @@ brew install --cask hyper
 brew install --cask iterm2
 brew install --cask keepingyouawake
 brew install --cask macdown
-brew install --cask nextcloud
 brew install --cask oversight
 brew install --cask postico
-brew install --cask pritunl
 brew install --cask pycharm
 brew install --cask slack
-brew install --cask steam
 brew install --cask spectacle
-brew install --cask transmission
 brew install --cask viscosity
 brew install --cask visual-studio-code
 
