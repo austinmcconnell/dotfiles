8f96c4b85b1a2bc75cc8fd8ccfc88b6b03d22697
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-10-21 15:39:59 -0500

    Move brew cask installs to brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -63,4 +63,31 @@ brew install --cask quicklook-csv
 brew install --cask qlimagesize
 brew install --cask webpquicklook
 
+# Install applications
+brew install --cask atom
+brew install --cask betterzip
+brew install --cask docker
+brew install --cask dozer
+brew install --cask dropbox
+brew install --cask evernote
+brew install --cask flux
+brew install --cask font-source-code-pro
+brew install --cask google-chrome
+brew install --cask google-cloud-sdk
+brew install --cask gpg-suite
+brew install --cask hazel
+brew install --cask hyper
+brew install --cask iterm2
+brew install --cask keepingyouawake
+brew install --cask macdown
+brew install --cask oversight
+brew install --cask postico
+brew install --cask pycharm
+brew install --cask slack
+brew install --cask spectacle
+brew install --cask viscosity
+brew install --cask visual-studio-code
+
+# Create symlinks
+ln -sfv "$DOTFILES_DIR/etc/hyper/.hyper.js" ~
 ln -sfv "$DOTFILES_DIR/etc/misc/hadolint.yaml" ~/.config
--- macos/apps.sh
+++ macos/apps.sh
@@ -2,7 +2,7 @@
 
 if ! is-executable brew; then
     echo "**************************************************"
-    echo "Skipping macOS installs: Homebrew not installed"
+    echo "Skipping macOS app installs"
     echo "**************************************************"
     return
 else
@@ -10,33 +10,6 @@ else
     echo "Installing macOS apps"
     echo "**************************************************"
 fi
-
-# Install applications
-brew install --cask atom
-brew install --cask betterzip
-brew install --cask docker
-brew install --cask dozer
-brew install --cask dropbox
-brew install --cask evernote
-brew install --cask flux
-brew install --cask font-source-code-pro
-brew install --cask google-chrome
-brew install --cask google-cloud-sdk
-brew install --cask gpg-suite
-brew install --cask hazel
-brew install --cask hyper
-brew install --cask iterm2
-brew install --cask keepingyouawake
-brew install --cask macdown
-brew install --cask oversight
-brew install --cask postico
-brew install --cask pycharm
-brew install --cask slack
-brew install --cask spectacle
-brew install --cask viscosity
-brew install --cask visual-studio-code
-
-ln -sfv "$DOTFILES_DIR/etc/hyper/.hyper.js" ~
 
 # Mac App Store application
 mas install 1236045954 # Canary mail
