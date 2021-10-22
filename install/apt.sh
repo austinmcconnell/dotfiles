18295e9f0fe857d3add32e5a8e74b9198115031a
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-10-21 20:57:52 -0500

    Install kubernetes-cli using official instructions

--- install/apt.sh
+++ install/apt.sh
@@ -12,9 +12,16 @@ fi
 
 sudo apt install -y fonts-firacode
 sudo apt install -y jq
-sudo apt install -y kubernetes
 sudo apt install -y shellcheck
 sudo apt install -y tree
 sudo apt install -y yamllint
 
 sudo snap install shfmt
+
+# Install kubectl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
