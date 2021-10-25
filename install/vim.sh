ad27a4a821abda4760ef47d786d4e864248ae938
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-10-25 08:07:08 -0500

    Refactor using REPO_DIR in vim.sh

--- install/vim.sh
+++ install/vim.sh
@@ -45,144 +45,167 @@ ln -sfv "$DOTFILES_DIR/runcom/.ctags" ~
 ln -sfv "$DOTFILES_DIR/etc/yaml/yamllint" ~/.config/yamllint/config
 
 ## Add colorschemes
-if [ -d "$HOME/.vim/colors/darcula/.git" ]; then
-    git --work-tree="$HOME/.vim/colors/darcula" --git-dir="$HOME/.vim/colors/darcula/.git" pull origin master
+REPO_DIR="$HOME/.vim/colors/darcula"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/blueshirts/darcula "$HOME/.vim/colors/darcula/"
-    ln -sfv "$HOME/.vim/colors/darcula/colors/darcula.vim" "$HOME/.vim/colors/darcula.vim"
+    git clone https://github.com/blueshirts/darcula "$REPO_DIR"
+    ln -sfv "$REPO_DIR/colors/darcula.vim" "$HOME/.vim/colors/darcula.vim"
 fi
 
-if [ -d "$HOME/.vim/colors/solarized/.git" ]; then
-    git --work-tree="$HOME/.vim/colors/solarized" --git-dir="$HOME/.vim/colors/solarized/.git" pull origin master
+REPO_DIR="$HOME/.vim/colors/solarized"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/altercation/vim-colors-solarized "$HOME/.vim/colors/solarized/"
-    ln -sfv "$HOME/.vim/colors/solarized/colors/solarized.vim" "$HOME/.vim/colors/solarized.vim"
+    git clone https://github.com/altercation/vim-colors-solarized "$REPO_DIR"
+    ln -sfv "$REPO_DIR/colors/solarized.vim" "$HOME/.vim/colors/solarized.vim"
 fi
 
-if [ -d "$HOME/.vim/colors/nord/.git" ]; then
-    git --work-tree="$HOME/.vim/colors/nord" --git-dir="$HOME/.vim/colors/nord/.git" pull origin develop
+REPO_DIR="$HOME/.vim/colors/nord"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin develop
 else
-    git clone https://github.com/arcticicestudio/nord-vim "$HOME/.vim/colors/nord/"
-    ln -sfv "$HOME/.vim/colors/nord/colors/nord.vim" "$HOME/.vim/colors/nord.vim"
+    git clone https://github.com/arcticicestudio/nord-vim "$REPO_DIR"
+    ln -sfv "$REPO_DIR/colors/nord.vim" "$HOME/.vim/colors/nord.vim"
 fi
 
 ## Add plugins
-if [ -d "$HOME/.vim/pack/bundle/start/auto-pairs/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/auto-pairs" --git-dir="$HOME/.vim/pack/bundle/start/auto-pairs/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/auto-pairs"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/jiangmiao/auto-pairs.git "$HOME/.vim/pack/bundle/start/auto-pairs"
+    git clone https://github.com/jiangmiao/auto-pairs.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/nerdtree/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/nerdtree" --git-dir="$HOME/.vim/pack/bundle/start/nerdtree/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/nerdtree"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/preservim/nerdtree.git "$HOME/.vim/pack/bundle/start/nerdtree"
+    git clone https://github.com/preservim/nerdtree.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/nerdtree-git/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/nerdtree-git" --git-dir="$HOME/.vim/pack/bundle/start/nerdtree-git/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/nerdtree-git"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git "$HOME/.vim/pack/bundle/start/nerdtree-git"
+    git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/tagbar/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/tagbar" --git-dir="$HOME/.vim/pack/bundle/start/tagbar/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/tagbar"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/majutsushi/tagbar.git "$HOME/.vim/pack/bundle/start/tagbar"
+    git clone https://github.com/majutsushi/tagbar.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/gitgutter/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/gitgutter" --git-dir="$HOME/.vim/pack/bundle/start/gitgutter/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/gitgutter"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/airblade/vim-gitgutter.git "$HOME/.vim/pack/bundle/start/gitgutter"
+    git clone https://github.com/airblade/vim-gitgutter.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/lightline/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/lightline" --git-dir="$HOME/.vim/pack/bundle/start/lightline/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/lightline"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/itchyny/lightline.vim.git "$HOME/.vim/pack/bundle/start/lightline"
+    git clone https://github.com/itchyny/lightline.vim.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/fugitive/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/fugitive" --git-dir="$HOME/.vim/pack/bundle/start/fugitive/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/fugitive"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/tpope/vim-fugitive.git "$HOME/.vim/pack/bundle/start/fugitive"
+    git clone https://github.com/tpope/vim-fugitive.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/gutentags/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/gutentags" --git-dir="$HOME/.vim/pack/bundle/start/gutentags/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/gutentags"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/ludovicchabant/vim-gutentags.git "$HOME/.vim/pack/bundle/start/gutentags"
+    git clone https://github.com/ludovicchabant/vim-gutentags.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/ale/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/ale" --git-dir="$HOME/.vim/pack/bundle/start/ale/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/ale"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/w0rp/ale.git "$HOME/.vim/pack/bundle/start/ale"
+    git clone https://github.com/w0rp/ale.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/lightline-ale/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/lightline-ale" --git-dir="$HOME/.vim/pack/bundle/start/lightline-ale/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/lightline-ale"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/maximbaz/lightline-ale.git "$HOME/.vim/pack/bundle/start/lightline-ale"
+    git clone https://github.com/maximbaz/lightline-ale.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/goyo/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/goyo" --git-dir="$HOME/.vim/pack/bundle/start/goyo/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/goyo"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/junegunn/goyo.vim.git "$HOME/.vim/pack/bundle/start/goyo"
+    git clone https://github.com/junegunn/goyo.vim.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/limelight/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/limelight" --git-dir="$HOME/.vim/pack/bundle/start/limelight/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/limelight"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/junegunn/limelight.vim.git "$HOME/.vim/pack/bundle/start/limelight"
+    git clone https://github.com/junegunn/limelight.vim.git "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/undotree/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/undotree" --git-dir="$HOME/.vim/pack/bundle/start/undotree/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/undotree"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/mbbill/undotree "$HOME/.vim/pack/bundle/start/undotree"
+    git clone https://github.com/mbbill/undotree "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/buftabline/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/buftabline" --git-dir="$HOME/.vim/pack/bundle/start/buftabline/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/buftabline"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/ap/vim-buftabline "$HOME/.vim/pack/bundle/start/buftabline"
+    git clone https://github.com/ap/vim-buftabline "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/grepper/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/grepper" --git-dir="$HOME/.vim/pack/bundle/start/grepper/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/grepper"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/mhinz/vim-grepper "$HOME/.vim/pack/bundle/start/grepper"
+    git clone https://github.com/mhinz/vim-grepper "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/hardtime/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/hardtime" --git-dir="$HOME/.vim/pack/bundle/start/hardtime/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/hardtime"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/takac/vim-hardtime "$HOME/.vim/pack/bundle/start/hardtime"
+    git clone https://github.com/takac/vim-hardtime "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/indentline/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/indentline" --git-dir="$HOME/.vim/pack/bundle/start/indentline/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/indentline"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/Yggdroot/indentLine "$HOME/.vim/pack/bundle/start/indentline"
+    git clone https://github.com/Yggdroot/indentLine "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/yaml-folds/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/yaml-folds" --git-dir="$HOME/.vim/pack/bundle/start/yaml-folds/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/yaml-folds"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/pedrohdz/vim-yaml-folds "$HOME/.vim/pack/bundle/start/yaml-folds"
+    git clone https://github.com/pedrohdz/vim-yaml-folds "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/list-toggle/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/list-toggle" --git-dir="$HOME/.vim/pack/bundle/start/list-toggle/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/list-toggle"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/Valloric/ListToggle "$HOME/.vim/pack/bundle/start/list-toggle"
+    git clone https://github.com/Valloric/ListToggle "$REPO_DIR"
 fi
 
-if [ -d "$HOME/.vim/pack/bundle/start/supertab/.git" ]; then
-    git --work-tree="$HOME/.vim/pack/bundle/start/supertab" --git-dir="$HOME/.vim/pack/bundle/start/supertab/.git" pull origin master
+REPO_DIR="$HOME/.vim/pack/bundle/start/supertab"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
-    git clone https://github.com/ervandew/supertab "$HOME/.vim/pack/bundle/start/supertab"
+    git clone https://github.com/ervandew/supertab "$REPO_DIR"
 fi
