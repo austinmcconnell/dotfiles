5fc26c0c9c3a85eaf30b31101a707f76d99b09c8
Austin McConnell <austin.s.mcconnell@gmail.com>
2022-02-23 07:47:46 -0600

    Link python scripts to /scripts directory under ~/projects

--- install/python.sh
+++ install/python.sh
@@ -28,14 +28,15 @@ fi
 mkdir -p ~/.config/proselint
 mkdir -p ~/.config/mypy
 mkdir -p ~/.config/isort/
+mkdir -p ~/projects/scripts
 
 ln -sfv "$DOTFILES_DIR/etc/python/flake8" ~/.config
 ln -sfv "$DOTFILES_DIR/etc/python/pylintrc" ~/.config
 ln -sfv "$DOTFILES_DIR/etc/python/proselint" ~/.config/proselint/config
 ln -sfv "$DOTFILES_DIR/etc/python/mypy" ~/.config/mypy/config
 ln -sfv "$DOTFILES_DIR/etc/python/.isort.cfg" ~/.config/isort/.isort.cfg
-ln -sfv "$DOTFILES_DIR/scripts/reinitialize-git-repositories.py" ~/projects
-ln -sfv "$DOTFILES_DIR/scripts/sort-git-repos-by-owner.py" ~/projects
+ln -sfv "$DOTFILES_DIR/scripts/reinitialize-git-repositories.py" ~/projects/scripts
+ln -sfv "$DOTFILES_DIR/scripts/sort-git-repos-by-owner.py" ~/projects/scripts
 
 DEFAULT_PYTHON_VERSION=3.7.4
 
