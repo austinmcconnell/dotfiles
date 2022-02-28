a8d9c360246628e9899d457d4627040922c575c8
Austin McConnell <austin.s.mcconnell@gmail.com>
2022-02-27 19:36:27 -0600

    Remove unused DOTFILES_CACHE line

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Get current dir (so run this script from anywhere)
 export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
 DOTFILES_EXTRA_DIR="$HOME/.extra"
 
 # Make utilities available
