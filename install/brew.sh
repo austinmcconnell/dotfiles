1c2c622fcf0c40129844dd2a26723e9c3151b46e
Austin McConnell <austin.s.mcconnell@gmail.com>
2022-08-02 08:20:11 -0500

    Switch out intel-only apps for apple silicon or universal apps

--- install/brew.sh
+++ install/brew.sh
@@ -44,7 +44,7 @@ brew install --formula openssl # Used for compiling (e.g. pyenv building python 
 brew install --formula shellcheck
 brew install --formula shfmt
 brew install --formula ssh-copy-id
-brew install --formula svn
+brew install --formula svn # Only needed to install font-source-code-pro. Safe to delete after font installed.
 brew install --formula terraform
 brew install --formula tree
 brew install --formula unar
@@ -55,11 +55,8 @@ brew install --formula zlib # Used for compiling (e.g. pyenv building python ver
 
 # Install applications
 brew install --cask alfred
-brew install --cask atom
-brew install --cask betterzip
+brew install --cask bartender
 brew install --cask docker
-brew install --cask dozer
-brew install --cask dropbox
 brew install --cask evernote
 brew install --cask flux
 brew install --cask font-source-code-pro
@@ -68,20 +65,20 @@ brew install --cask google-chrome
 brew install --cask google-cloud-sdk
 brew install --cask gpg-suite
 brew install --cask hazel
-brew install --cask hyper
 brew install --cask iterm2
 brew install --cask keepingyouawake
-brew install --cask keybase
-brew install --cask macdown
 brew install --cask monitorcontrol
 brew install --cask multipass
 brew install --cask oversight
 brew install --cask postico
+brew install --cask rectangle
+brew install --cask silicon
 brew install --cask slack
-brew install --cask spectacle
 brew install --cask spotify
+brew install --cask sublime-text
 brew install --cask steam
 brew install --cask tableplus
+brew install --cask the-unarchiver
 brew install --cask vagrant
 brew install --cask viscosity
 brew install --cask visual-studio-code
