4950e547df00c8d8c58d1fea43d165249aa4ac59
Austin McConnell <austin.s.mcconnell@gmail.com>
2022-08-24 14:38:14 -0500

    Move vim git clone targers into common ~/.repositories directory and symlink to where needed

--- install/vim.sh
+++ install/vim.sh
@@ -45,7 +45,7 @@ ln -sfv "$DOTFILES_DIR/runcom/.ctags" ~
 ln -sfv "$DOTFILES_DIR/etc/yaml/yamllint" ~/.config/yamllint/config
 
 ## Add colorschemes
-REPO_DIR="$HOME/.vim/colors/darcula"
+REPO_DIR="$HOME/.repositories/darcula"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
@@ -53,7 +53,7 @@ else
     ln -sfv "$REPO_DIR/colors/darcula.vim" "$HOME/.vim/colors/darcula.vim"
 fi
 
-REPO_DIR="$HOME/.vim/colors/solarized"
+REPO_DIR="$HOME/.repositories/solarized"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
@@ -61,7 +61,7 @@ else
     ln -sfv "$REPO_DIR/colors/solarized.vim" "$HOME/.vim/colors/solarized.vim"
 fi
 
-REPO_DIR="$HOME/.vim/colors/nord"
+REPO_DIR="$HOME/.repositories/nord"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin develop
 else
@@ -70,142 +70,162 @@ else
 fi
 
 ## Add plugins
-REPO_DIR="$HOME/.vim/pack/bundle/start/auto-pairs"
+REPO_DIR="$HOME/.repositories/auto-pairs"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/jiangmiao/auto-pairs.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/auto-pairs"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/nerdtree"
+REPO_DIR="$HOME/.repositories/nerdtree"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/preservim/nerdtree.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/nerdtree"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/nerdtree-git"
+REPO_DIR="$HOME/.repositories/nerdtree-git"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/nerdtree-git"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/tagbar"
+REPO_DIR="$HOME/.repositories/tagbar"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/majutsushi/tagbar.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/tagbar"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/gitgutter"
+REPO_DIR="$HOME/.repositories/gitgutter"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/airblade/vim-gitgutter.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/gitgutter"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/lightline"
+REPO_DIR="$HOME/.repositories/lightline"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/itchyny/lightline.vim.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/lightline"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/fugitive"
+REPO_DIR="$HOME/.repositories/fugitive"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/tpope/vim-fugitive.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/fugitive"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/gutentags"
+REPO_DIR="$HOME/.repositories/gutentags"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/ludovicchabant/vim-gutentags.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/gutentags"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/ale"
+REPO_DIR="$HOME/.repositories/ale"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/w0rp/ale.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/ale"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/lightline-ale"
+REPO_DIR="$HOME/.repositories/lightline-ale"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/maximbaz/lightline-ale.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/lightline-ale"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/goyo"
+REPO_DIR="$HOME/.repositories/goyo"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/junegunn/goyo.vim.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/goyo"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/limelight"
+REPO_DIR="$HOME/.repositories/limelight"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/junegunn/limelight.vim.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/limelight"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/undotree"
+REPO_DIR="$HOME/.repositories/undotree"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/mbbill/undotree "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/undotree"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/buftabline"
+REPO_DIR="$HOME/.repositories/buftabline"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/ap/vim-buftabline "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/buftabline"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/grepper"
+REPO_DIR="$HOME/.repositories/grepper"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/mhinz/vim-grepper "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/grepper"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/hardtime"
+REPO_DIR="$HOME/.repositories/hardtime"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/takac/vim-hardtime "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/hardtime"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/indentline"
+REPO_DIR="$HOME/.repositories/indentline"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/Yggdroot/indentLine "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/indentline"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/yaml-folds"
+REPO_DIR="$HOME/.repositories/yaml-folds"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/pedrohdz/vim-yaml-folds "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/yaml-folds"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/list-toggle"
+REPO_DIR="$HOME/.repositories/list-toggle"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/Valloric/ListToggle "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/list-toggle"
 fi
 
-REPO_DIR="$HOME/.vim/pack/bundle/start/supertab"
+REPO_DIR="$HOME/.repositories/supertab"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
 else
     git clone https://github.com/ervandew/supertab "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$HOME/.vim/pack/bundle/start/supertab"
 fi
