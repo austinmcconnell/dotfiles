783ba10df00911bbe8c232c881bd8e72beee8ed4
Austin McConnell <austin.s.mcconnell@gmail.com>
2022-09-02 11:30:34 -0500

    Add default pip.conf with trusted-hosts

--- install/python.sh
+++ install/python.sh
@@ -25,12 +25,14 @@ else
     fi
 fi
 
+mkdir -p ~/.config/pip
 mkdir -p ~/.config/proselint
 mkdir -p ~/.config/mypy
 mkdir -p ~/.config/isort/
 mkdir -p ~/projects/scripts
 mkdir -p ~/.git-templates
 
+ln -sfv "$DOTFILES_DIR/etc/python/pip.conf" ~/.config/pip
 ln -sfv "$DOTFILES_DIR/etc/python/flake8" ~/.config
 ln -sfv "$DOTFILES_DIR/etc/python/pylintrc" ~/.config
 ln -sfv "$DOTFILES_DIR/etc/python/proselint" ~/.config/proselint/config
