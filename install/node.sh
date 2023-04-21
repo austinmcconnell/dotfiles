ce4c951fba115276031d7a08eee4ec4ef380090a
Austin McConnell <austin.s.mcconnell@gmail.com>
2023-04-21 14:37:06 -0500

    Move node.sh symlinks above git clones

--- install/node.sh
+++ install/node.sh
@@ -12,6 +12,9 @@ fi
 
 NVM_DIR="$HOME/.nvm"
 
+ln -sfv "$DOTFILES_DIR/etc/node/default-packages" "$NVM_DIR"
+ln -sfv "$DOTFILES_DIR/etc/node/markdownlint" ~/.markdownlintrc
+
 if [ -d "$NVM_DIR/.git" ]; then
     git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" fetch --tags origin
     git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" checkout $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" describe --abbrev=0 --tags --match "v[0-9]*" $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" rev-list --tags --max-count=1))
@@ -19,9 +22,6 @@ else
     git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
     git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" checkout $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" describe --abbrev=0 --tags --match "v[0-9]*" $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" rev-list --tags --max-count=1))
 fi
-
-ln -sfv "$DOTFILES_DIR/etc/node/markdownlint" ~/.markdownlintrc
-ln -sfv "$DOTFILES_DIR/etc/node/default-packages" "$NVM_DIR"
 
 . "$NVM_DIR/nvm.sh"
 nvm install --lts
