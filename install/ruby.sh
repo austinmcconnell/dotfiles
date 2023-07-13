8c4c479a9d7e0eb31404b3c8a3c604224ff5229e
Austin McConnell <austin.s.mcconnell@gmail.com>
2023-07-13 12:04:34 -0500

    Add ruby default-gems list and plugin

--- install/ruby.sh
+++ install/ruby.sh
@@ -25,9 +25,21 @@ fi
 
 DEFAULT_RUBY_VERSION=3.2.2
 
-mkdir -p ~/.config/rubocop
+RBENV_DIR=$(rbenv root)
 
+mkdir -p ~/.config/rubocop
+mkdir -p "$RBENV_DIR/plugins"
+
+ln -sfv "$DOTFILES_DIR/etc/ruby/default-gems" "$RBENV_DIR/default-gems"
 ln -sfv "$DOTFILES_DIR/etc/ruby/rubocop.yml" ~/.config/rubocop/config.yml
+
+REPO_DIR="$HOME/.repositories/rbenv-default-gems"
+if [ -d "$REPO_DIR/.git" ]; then
+    git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
+else
+    git clone https://github.com/rbenv/rbenv-default-gems.git "$REPO_DIR"
+    ln -sfv "$REPO_DIR" "$RBENV_DIR/plugins/rbenv-default-gems"
+fi
 
 rbenv install "${DEFAULT_RUBY_VERSION}"
 rbenv global "${DEFAULT_RUBY_VERSION}"
