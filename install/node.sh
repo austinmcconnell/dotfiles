290d8e8ac97aa28b675c9014a2194fd3104b3f92
Austin McConnell <austin.s.mcconnell@gmail.com>
2023-07-13 15:09:24 -0500

    Move node symlink creation after nvm clone into NVM_DIR

--- install/node.sh
+++ install/node.sh
@@ -12,10 +12,6 @@ fi
 
 NVM_DIR="$HOME/.nvm"
 
-ln -sfv "$DOTFILES_DIR/etc/node/default-packages" "$NVM_DIR"
-ln -sfv "$DOTFILES_DIR/etc/node/markdownlint" ~/.markdownlintrc
-ln -sfv "$DOTFILES_DIR/etc/node/prettier.toml" ~/.config/prettier.toml
-
 if [ -d "$NVM_DIR/.git" ]; then
     git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" fetch --tags origin
     # shellcheck disable=SC2046
@@ -25,6 +21,11 @@ else
     # shellcheck disable=SC2046
     git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" checkout $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" describe --abbrev=0 --tags --match "v[0-9]*" $(git --work-tree="$NVM_DIR" --git-dir="$NVM_DIR/.git" rev-list --tags --max-count=1))
 fi
+
+# Due to nvm being cloned into the .nvm directory, need to clone first before creating symlinks
+ln -sfv "$DOTFILES_DIR/etc/node/default-packages" "$NVM_DIR"
+ln -sfv "$DOTFILES_DIR/etc/node/markdownlint" ~/.markdownlintrc
+ln -sfv "$DOTFILES_DIR/etc/node/prettier.toml" ~/.config/prettier.toml
 
 # shellcheck source-path=SCRIPTDIR/../../.nvm
 . "$NVM_DIR/nvm.sh"
