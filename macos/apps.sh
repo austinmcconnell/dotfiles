a318a55abd15163975a45af8b898001a1cd146fa
Austin McConnell <austin.s.mcconnell@gmail.com>
2023-11-02 10:09:11 -0500

    Add Weather for Status Bar to apps.sh

--- macos/apps.sh
+++ macos/apps.sh
@@ -17,6 +17,7 @@ mas install 926036361  # Lastpass
 mas install 1289583905 # Pixelmator Pro
 mas install 1303222628 # Paprika Recipe Manager 3
 mas install 714196447  # Menubar Stats
+mas install 1448031908 # Weather for Status Bar
 
 # Safari Extensions
 mas install 1481669779 # Evernote Web Clipper
