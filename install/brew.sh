9124ed00191efbf417883ada333e5c628e180738
Austin McConnell <austin.s.mcconnell@gmail.com>
2024-02-20 09:15:39 -0600

    Add bat config file

--- install/brew.sh
+++ install/brew.sh
@@ -17,6 +17,7 @@ else
     fi
 fi
 
+mkdir -p ~/.config/bat
 mkdir -p ~/.config/fd
 mkdir -p ~/.config/httpie
 
@@ -24,6 +25,7 @@ ln -sfv "$DOTFILES_DIR/etc/misc/hadolint.yaml" ~/.config
 ln -sfv "$DOTFILES_DIR/etc/misc/shellcheckrc" ~/.config/shellcheckrc
 ln -sfv "$DOTFILES_DIR/etc/fd/ignore" ~/.config/fd/ignore
 ln -sfv "$DOTFILES_DIR/etc/httpie/config.json" ~/.config/httpie/config.json
+ln -sfv "$DOTFILES_DIR/etc/bat/config" ~/.config/bat/config
 ln -sfv "$DOTFILES_DIR/etc/sublime-text/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
 ln -sfv "$DOTFILES_DIR/etc/sublime-text/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
 ln -sfv "$DOTFILES_DIR/etc/sublime-text/Python.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
