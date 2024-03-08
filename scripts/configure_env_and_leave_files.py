ccbc9108bc4affb4e93fda789aaf4a879c230205
Austin McConnell <austin.s.mcconnell@gmail.com>
2024-03-08 14:22:38 -0600

    Add script to configure .env and .env.leave files for automatically sourcing and deactivating python virtual environments

---
+++ scripts/configure_env_and_leave_files.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+REPOSITORIES_DIR = Path.home() / 'projects'
+
+# This assumes git repos have been sorted by owner as in sort-git-repos-by-owner.py
+owner_dirs = sorted([x for x in REPOSITORIES_DIR.iterdir() if x.is_dir()])
+
+projects = []
+for project_dir in owner_dirs:
+    projects.extend([y for y in project_dir.iterdir() if y.is_dir()])
+projects = sorted(projects)
+
+for project in projects:
+
+    print('*' * 70)
+    print(f'Checking project: {project}')
+    print('*' * 70)
+
+    git_dir = project / '.git'
+
+    if not git_dir.exists():
+        print('Not a git repo. Skipping...')
+        continue
+
+    venv_dir = project / '.venv'
+
+    if not venv_dir.exists():
+        print('No .venv directory found. Skipping...')
+        continue
+
+    env_file = project / '.env'
+    env_leave_file = project / '.env.leave'
+
+    if env_file.exists():
+        contents = env_file.read_text()
+        lines = contents.split('\n')
+
+        if 'source .venv/bin/activate' in lines:
+            print('.env file looks good')
+        else:
+            print('source .venv/bin/activate not found. Adding...')
+            lines.append('# Activate virtual environment')
+            lines.append('source .venv/bin/activate')
+
+            env_file.write_text('\n'.join(lines))
+    else:
+        print('.env not found. Adding...')
+        lines = ['# Activate virtual environment', 'source .venv/bin/activate']
+        env_file.write_text('\n'.join(lines))
+
+    if env_leave_file.exists():
+        contents = env_leave_file.read_text()
+        lines = contents.split('\n')
+
+        if 'deactivate' in lines:
+            print('.env.leave file looks good')
+        else:
+            print('deactivate not found. Adding...')
+            lines.append('# Deactivate virtual environment')
+            lines.append('deactivate')
+
+            env_leave_file.write_text('\n'.join(lines))
+    else:
+        print('.env.leave not found. Adding...')
+        lines = ['# Deactivate virtual environment', 'deactivate']
+        env_leave_file.write_text('\n'.join(lines))
