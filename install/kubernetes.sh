cee5987b9312f06fd652d8834e2ad3d0ef00821d
Austin McConnell <austin.s.mcconnell@gmail.com>
2024-10-23 09:16:24 -0500

    Switch to kind to create kubernetes cluster

--- install/kubernetes.sh
+++ install/kubernetes.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-if is-executable microk8s; then
+if is-executable kind; then
     echo "**************************************************"
     echo "Configuring kubernetes"
     echo "**************************************************"
-    microk8s start
 else
-    KUBERNETES_VERSION=1.28/stable
     if is-macos; then
         echo "**************************************************"
         echo "Installing Kubernetes"
         echo "**************************************************"
-        brew install ubuntu/microk8s/microk8s kubectl kubectx
-        microk8s install --cpu=4 --memory=8 --channel=$KUBERNETES_VERSION
-        # If the install step fails, try disabling stealth mode in Security & Privacy -> Firewall -> Firewall Options, install again, then re-enable stealth mode
+        brew install kind kubectl kubectx
     elif is-debian; then
         echo "**************************************************"
         echo "Installing Kubernetes"
         echo "**************************************************"
-        sudo snap install microk8s --classic --channel=$KUBERNETES_VERSION
-        sudo apt install -y kubectl
+        sudo apt install -y kind kubectl
     else
         echo "**************************************************"
         echo "Skipping Kubernetes installation: Unidentified OS"
@@ -28,15 +23,13 @@ else
     fi
 fi
 
-microk8s status --wait-ready
+KUBERNETES_VERSION=v1.29.2
 
-microk8s enable dashboard dns registry istio ingress prometheus
+existing_clusters=$(kind get clusters --quiet)
 
-kubectl config set-cluster microk8s \
-    --server="$(microk8s kubectl config view --raw -o 'jsonpath={.clusters[0].cluster.server}')" \
-    --insecure-skip-tls-verify
-kubectl config set-credentials microk8s-admin \
-    --token="$(microk8s kubectl config view --raw -o 'jsonpath={.users[0].user.token}')"
-kubectl config set-context microk8s --cluster=microk8s --namespace=default --user=microk8s-admin
-
-microk8s stop
+if [[ $existing_clusters =~ "kind" ]]; then
+    kubectl cluster-info --context kind-kind
+else
+    kind create cluster --wait 3m --config "$DOTFILES_DIR/etc/kind/cluster-config.yaml" --image "kindest/node:$KUBERNETES_VERSION"
+    kubectl cluster-info --context kind-kind
+fi
