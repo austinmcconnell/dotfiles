52d3e055d7e75072a845c4445d6e57f8be3dfa79
Austin McConnell <austin.s.mcconnell@gmail.com>
2024-11-09 09:32:09 -0600

    Try restarting docker-mac-net-connect if it isnt working

--- etc/kind/test/test-docker-mac-net-connect.sh
+++ etc/kind/test/test-docker-mac-net-connect.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+ping_by_container_ip() {
+    echo "Pinging container using conainer ip $CONTAINER_IP"
+    if curl --connect-timeout 5 --retry 3 "$CONTAINER_IP" | grep -q 'nginx'; then
+        echo "Successfully pinged container"
+        exit_code=0
+    else
+        echo "Failed to ping container"
+        exit_code=1
+    fi
+}
+
 echo "Starting nginx container"
 docker run --rm --name nginx -d nginx
 
 CONTAINER_IP=$(docker inspect nginx --format '{{.NetworkSettings.IPAddress}}')
 
-echo "Pinging container using conainer ip $CONTAINER_IP"
-if curl --connect-timeout 5 --retry 3 "$CONTAINER_IP" | grep -q 'nginx'; then
-    echo "Successfully pinged container"
-    exit_code=0
-else
-    echo "Failed to ping container"
-    exit_code=1
+ping_by_container_ip
+
+if ((exit_code > 0)); then
+    echo "docker-mac-net-connect might be stuck. Enter password to try restarting service"
+    sudo brew services restart docker-mac-net-connect
+    ping_by_container_ip
 fi
 
 echo "Stoping nginx container"
