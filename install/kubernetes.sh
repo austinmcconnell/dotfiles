36e48b0ec48291f23e350c9d285819b88ee1910f
Austin McConnell <austin.s.mcconnell@gmail.com>
2024-11-12 08:09:20 -0600

    Switch kubernetes.sh set up cluster with helmsman

--- install/kubernetes.sh
+++ install/kubernetes.sh
@@ -11,7 +11,7 @@ else
         echo "**************************************************"
         echo "Installing Kubernetes"
         echo "**************************************************"
-        brew install kind kubectl kubectx helm mkcert
+        brew install kind kubectl kubectx helm mkcert helmsman
     elif is-debian; then
         echo "**************************************************"
         echo "Installing Kubernetes"
@@ -73,6 +73,14 @@ update_etc_hosts() {
     fi
 }
 
+add_hosts_entries() {
+    print_section_header "Adding /etc/hosts entries"
+    update_etc_hosts "$LOCAL_DOMAIN"
+    update_etc_hosts "prometheus.$LOCAL_DOMAIN"
+    update_etc_hosts "grafana.$LOCAL_DOMAIN"
+    update_etc_hosts "alertmanager.$LOCAL_DOMAIN"
+}
+
 install_docker_mac_net_connect() {
     # Allows connecting directly to Docker-for-Mac container via their IP address
     # https://github.com/chipmk/docker-mac-net-connect
@@ -93,10 +101,11 @@ install_docker_mac_net_connect() {
 create_kind_cluster() {
     print_section_header "Creating kind cluster"
 
-    existing_clusters=$(kind get clusters --quiet)
+    NAME=$1
 
-    if [[ $existing_clusters =~ "kind" ]]; then
-        kubectl cluster-info --context kind-kind
+    if kind get clusters --quiet | grep --quiet "$NAME"; then
+        echo "kind cluster already created"
+        kubectl cluster-info --context "kind-$NAME"
     else
         if docker network ls --filter name=kind | grep --quiet "kind"; then
             echo "Removing old kind docker network"
@@ -107,17 +116,17 @@ create_kind_cluster() {
         kind create cluster \
             --wait 3m \
             --config "$DOTFILES_DIR/etc/kind/cluster-config.yaml" \
-            --image "kindest/node:$KUBERNETES_VERSION"
-        kubectl cluster-info --context kind-kind
-    fi
+            --image "kindest/node:$KUBERNETES_VERSION" \
+            --name "$NAME"
+        kubectl cluster-info --context "kind-$NAME"
 
-    print_section_header "Testing NodePort"
-    sh "$DOTFILES_DIR/etc/kind/test/test-node-port.sh"
+        update_ca_certificates "$NAME"
+    fi
 }
 
 update_ca_certificates() {
     print_section_header "Updating mounted certificates"
-    nodes=$(kind get nodes)
+    nodes=$(kind get nodes --name "$1")
     for node in $nodes; do
         echo "Updating certs on node: $node"
         docker exec "$node" update-ca-certificates
@@ -125,243 +134,73 @@ update_ca_certificates() {
     done
 }
 
-install_ingress_nginx() {
-    print_section_header "Installing ingress-nginx"
-
-    if helm repo list | grep --quiet ingress-nginx; then
-        echo "ingress-nginx repo already added"
-    else
-        helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-        helm repo update
-    fi
-
-    if helm list --namespace ingress-nginx | grep --quiet ingress-nginx; then
-        echo "ingress-nginx already installed"
-        if [[ -n $(helm diff upgrade --reuse-values --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx) ]]; then
-            echo "Upgrading ingress-nginx"
-            helm upgrade --reuse-values --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx
-        else
-            echo "No upgrade needed"
-        fi
-    else
-        helm install \
-            --wait \
-            --timeout 5m \
-            --namespace ingress-nginx \
-            --create-namespace \
-            ingress-nginx ingress-nginx/ingress-nginx
-        kubectl wait --namespace ingress-nginx \
-            --for=condition=ready pod \
-            --selector=app.ingress-nginx.io/component=controller \
-            --timeout=90s
-
-        update_etc_hosts "$LOCAL_DOMAIN"
-    fi
-
-    print_section_header "Testing ingress-nginx"
-    sh "$DOTFILES_DIR/etc/kind/test/test-ingress-nginx-port-forward.sh"
-    sh "$DOTFILES_DIR/etc/kind/test/test-ingress-nginx-hosts-entry.sh"
-}
-
-install_prometheus() {
-    print_section_header "Install prometheus stack"
-
-    if helm repo list | grep --quiet prometheus-community; then
-        echo "prometheus-community repo already added"
-    else
-        helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-        helm repo update
-    fi
-
-    if helm list --namespace monitoring | grep --quiet prometheus; then
-        echo "prometheus already installed"
-    else
-        sed 's/.example/.'"$LOCAL_DOMAIN"'/g' "$DOTFILES_DIR/etc/kind/kube-prometheus-stack.yaml" |
-            helm install \
-                --wait \
-                --timeout 5m \
-                --namespace monitoring \
-                --create-namespace \
-                prometheus prometheus-community/kube-prometheus-stack \
-                --values -
-        kubectl wait --namespace monitoring \
-            --for=condition=ready pod \
-            --selector=app=kube-prometheus-stack-operator \
-            --timeout=90s
-
-        update_etc_hosts "prometheus.$LOCAL_DOMAIN"
-        update_etc_hosts "grafana.$LOCAL_DOMAIN"
-        update_etc_hosts "alertmanager.$LOCAL_DOMAIN"
-    fi
-}
-
-install_metallb() {
-    print_section_header "Installing metallb"
+configure_metallb() {
+    print_section_header "Configuring metallb"
     # helpful link: https://gist.github.com/RafalSkolasinski/b41b790b1c575223251ff90311419863
 
-    if helm repo list | grep --quiet metallb; then
-        echo "metallb repo already added"
+    kubectl rollout --namespace metallb-system status deployment metallb-controller
+
+    if kubectl get --namespace metallb-system ipaddresspools.metallb.io | grep --quiet local-pool; then
+        echo "IPAddressPool and L2Advertisement already created"
     else
-        helm repo add metallb https://metallb.github.io/metallb
-        helm repo update
-    fi
-
-    if helm list --namespace metallb-system | grep --quiet metallb; then
-        echo "metallb already installed"
-        if [[ -n $(helm diff upgrade --reuse-values --namespace metallb-system metallb metallb/metallb) ]]; then
-            echo "Upgrading ingress-nginx"
-            helm upgrade --reuse-values --namespace metallb-system metallb metallb/metallb
-        else
-            echo "No upgrade needed"
-        fi
-    else
-        helm install metallb metallb/metallb -n metallb-system --create-namespace
-        kubectl rollout --namespace metallb-system status deployment metallb-controller
-
-        # Gather network information
-        kind_network=$(docker network inspect kind | jq --raw-output '.[0].IPAM.Config[1].Subnet')
-        echo "Kind network: $kind_network"
-        network_gateway=$(docker network inspect kind | jq --raw-output '.[0].IPAM.Config[1].Gateway')
-        echo "Network gateway: $network_gateway"
-        kind_container_ip=$(docker inspect kind-control-plane --format '{{.NetworkSettings.Networks.kind.IPAddress}}')
-        echo "IP of container running kind: $kind_container_ip"
-
-        # Set available IP addresses
-        network_mask_start=$(echo "$kind_network" | cut -d / -f 1 | sed -e 's/\.0/\.255/' | sed -e 's/\.0/\.1/')
-        network_mask_end=$(echo "$kind_network" | cut -d / -f 1 | sed -e 's/\.0/\.255/g')
-        echo "Network mask: $network_mask_start-$network_mask_end"
-
-        bat "$DOTFILES_DIR/etc/kind/metallb-ipaddresspool.yaml" |
-            sed -e "s/1.1.1.1/$network_mask_start/" |
-            sed -e "s/2.2.2.2/$network_mask_end/" |
+        SUBNET=$(docker network inspect kind | jq --raw-output '.[0].IPAM.Config[1].Subnet') \
+            yq '(. | select(.kind == "IPAddressPool") | .spec.addresses.[0]) = strenv(SUBNET)' etc/kind/metallb-ipaddresspool.yaml |
             kubectl apply -f -
     fi
-
-    print_section_header "Testing metallb"
-    sh "$DOTFILES_DIR/etc/kind/test/test-metallb.sh"
 }
 
-install_cert_manager() {
-    print_section_header "Installing cert-manager"
+configure_cert_manager() {
+    print_section_header "Configure cert-manager"
 
-    if helm repo list | grep --quiet jetstack; then
-        echo "jetstack repo already added"
+    if kubectl get secrets --namespace cert-manager | grep --quiet mkcert-ca-key-pair; then
+        echo "mkcert-ca-key-pair already exists"
     else
-        helm repo add jetstack https://charts.jetstack.io
-        helm repo update
-    fi
-
-    if helm list --namespace cert-manager | grep --quiet cert-manager; then
-        echo "cert-manager already installed"
-        # helm diff support for --reset-then-reuse-values is merged in but not released yet
-        # https://github.com/databus23/helm-diff/pull/634
-        # if [[ -n $(helm diff upgrade --reset-then-reuse-values --namespace cert-manager cert-manager jetstack/cert-manager) ]]; then
-        #     echo "Upgrading cert-manager"
-        #     helm upgrade --reset-then-reuse-values --namespace cert-manager cert-manager jetstack/cert-manager
-        # else
-        #     echo "No upgrade needed"
-        # fi
-    else
-        helm install \
-            --namespace cert-manager \
-            --create-namespace \
-            --version v1.16.1 \
-            --values "$DOTFILES_DIR/etc/kind/cert-manager.yaml" \
-            cert-manager jetstack/cert-manager
-        kubectl rollout --namespace cert-manager status deployment cert-manager
-
         # Generate and install local CA
         mkcert -install
 
         # Add CA's certificate and key to kubernetes
         kubectl create secret tls mkcert-ca-key-pair \
             --key "$(mkcert -CAROOT)"/rootCA-key.pem \
-            --cert "$(mkcert -CAROOT)"/rootCA.pem -n cert-manager
+            --cert "$(mkcert -CAROOT)"/rootCA.pem --namespace cert-manager
 
         # Tell cert-manager to use this to issue certificates
         kubectl apply -f "$DOTFILES_DIR/etc/kind/cert-manager-cluster-issuer.yaml"
     fi
+}
+
+run_tests() {
+    print_section_header "Testing NodePort"
+    sh "$DOTFILES_DIR/etc/kind/test/test-node-port.sh"
+
+    print_section_header "Testing metallb"
+    sh "$DOTFILES_DIR/etc/kind/test/test-metallb.sh"
+
+    print_section_header "Testing ingress-nginx"
+    sh "$DOTFILES_DIR/etc/kind/test/test-ingress-nginx-port-forward.sh"
+    sh "$DOTFILES_DIR/etc/kind/test/test-ingress-nginx-hosts-entry.sh"
 
     print_section_header "Testing cert-manager"
     sh "$DOTFILES_DIR/etc/kind/test/test-cert-manager.sh"
-}
-
-install_metrics_server() {
-    print_section_header "Installing metrics-server"
-
-    if helm repo list | grep --quiet metrics-server; then
-        echo "metrics-server repo already added"
-    else
-        helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-        helm repo update
-    fi
-
-    if helm list --namespace cert-manager | grep --quiet cert-manager; then
-        echo "cert-manager installation found"
-    else
-        echo "cert-manager needs to be installed before you install metrics-server"
-        exit 1
-    fi
-
-    if helm list --namespace metrics-server | grep --quiet metrics-server; then
-        echo "metrics-server already installed"
-        if [[ -n $(helm diff upgrade --reuse-values --namespace metrics-server metrics-server metrics-server/metrics-server) ]]; then
-            echo "Upgrading metrics-server"
-            helm upgrade --reuse-values --namespace metrics-server metrics-server metrics-server/metrics-server
-        else
-            echo "No upgrade needed"
-        fi
-    else
-        helm install \
-            --wait \
-            --timeout 5m \
-            --namespace metrics-server \
-            --create-namespace \
-            --values "$DOTFILES_DIR/etc/kind/metrics-server.yaml" \
-            metrics-server metrics-server/metrics-server
-        kubectl wait --namespace metrics-server \
-            --for=condition=ready pod \
-            --selector=app.kubernetes.io/name=metrics-server \
-            --timeout=90s
-
-    fi
 
     print_section_header "Testing metrics-server"
     sh "$DOTFILES_DIR/etc/kind/test/test-metrics-server.sh"
     sh "$DOTFILES_DIR/etc/kind/test/test-horizontal-pod-autoscaler.sh"
 }
 
-update_helm_repos() {
-    print_section_header "Updating helm repos"
-    helm repo update
-}
+# install_docker_mac_net_connect
 
-install_helm_plugins() {
-    print_section_header "Installing helm plugins"
+create_kind_cluster "kind"
 
-    if helm plugin list | grep --quiet diff; then
-        echo "diff plugin already added"
-    else
-        helm plugin install https://github.com/databus23/helm-diff
-    fi
-}
+helmsman --apply -f etc/kind/cluster-infrastructure.toml --target metallb
 
-install_docker_mac_net_connect
+configure_metallb
 
-create_kind_cluster
+LOCAL_DOMAIN=$LOCAL_DOMAIN helmsman --apply --subst-env-values -f etc/kind/cluster-infrastructure.toml
 
-update_ca_certificates
+configure_cert_manager
 
-install_helm_plugins
+add_hosts_entries
 
-update_helm_repos
+run_tests
 
-install_metallb
-
-install_ingress_nginx
-
-install_cert_manager
-
-install_prometheus
-
-install_metrics_server
+helmsman --check-for-chart-updates
