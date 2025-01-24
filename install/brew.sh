d64cec097f5884f4b26bceeed0c386125cbd21c4
Austin McConnell <austin.s.mcconnell@gmail.com>
2025-01-24 09:11:28 -0600

    Add caching to brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -1,18 +1,99 @@
 #!/bin/bash
+
+set -euo pipefail
+
+BREW_INSTALLED_FORMULAE=""
+BREW_INSTALLED_CASKS=""
+BREW_OUTDATED_FORMULAE=""
+BREW_OUTDATED_CASKS=""
+BREW_TAPS=""
+CACHE_INITIALIZED=false
+
+init_brew_cache() {
+    echo "Initializing brew cache..."
+    BREW_INSTALLED_FORMULAE=$(brew list --formula 2>/dev/null)
+    BREW_INSTALLED_CASKS=$(brew list --cask 2>/dev/null)
+    BREW_OUTDATED_FORMULAE=$(brew outdated --formula 2>/dev/null)
+    BREW_OUTDATED_CASKS=$(brew outdated --cask 2>/dev/null)
+    BREW_TAPS=$(brew tap)
+    CACHE_INITIALIZED=true
+}
+
+is_package_installed() {
+    local package=$1
+    local package_type=${2:-formula}
+
+    if [[ "$package_type" == "formula" ]]; then
+        echo "$BREW_INSTALLED_FORMULAE" | grep -q "^$package\$"
+    else
+        echo "$BREW_INSTALLED_CASKS" | grep -q "^$package\$"
+    fi
+}
+
+is_package_outdated() {
+    local package=$1
+    local package_type=${2:-formula}
+
+    if [[ "$package_type" == "formula" ]]; then
+        echo "$BREW_OUTDATED_FORMULAE" | grep -q "^$package\$"
+    else
+        echo "$BREW_OUTDATED_CASKS" | grep -q "^$package\$"
+    fi
+}
+
+install_if_needed() {
+    local package=$1
+    local package_type=${2:-formula}
+
+    if [[ "$CACHE_INITIALIZED" != "true" ]]; then
+        init_brew_cache
+    fi
+
+    if ! is_package_installed "$package" "$package_type"; then
+        echo "Installing $package ($package_type)..."
+        if [[ "$package_type" == "cask" ]]; then
+            brew install --cask "$package"
+        else
+            brew install --formula "$package"
+        fi
+        init_brew_cache
+    elif is_package_outdated "$package" "$package_type"; then
+        echo "Updating $package ($package_type)..."
+        if [[ "$package_type" == "cask" ]]; then
+            brew upgrade --cask "$package"
+        else
+            brew upgrade --formula "$package"
+        fi
+        init_brew_cache
+    else
+        echo "$package ($package_type) is already installed and up to date"
+    fi
+}
+
+tap_if_needed() {
+    local tap=$1
+
+    if [[ "$CACHE_INITIALIZED" != "true" ]]; then
+        init_brew_cache
+    fi
+
+    if ! echo "$BREW_TAPS" | grep -q "^$tap\$"; then
+        echo "Tapping $tap..."
+        brew tap "$tap"
+        init_tap_cache
+    else
+        echo "Tap $tap already exists"
+    fi
+}
+
 if is-executable brew; then
-    echo "**************************************************"
-    echo "Installing macOS services"
-    echo "**************************************************"
+    print_header "Installing brew formulas and casks"
 else
     if is-macos; then
-        echo "**************************************************"
-        echo "Installing Homebrew"
-        echo "**************************************************"
+        print_header "Installing Homebrew"
         NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
-        echo "**************************************************"
-        echo "Skipping Homebrew installation: Not macOS"
-        echo "**************************************************"
+        print_header "Skipping Homebrew installation: Not macOS"
         return
     fi
 fi
@@ -34,106 +115,114 @@ if is-macos; then
     ln -sfv "$DOTFILES_DIR/etc/sublime-text/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text/Packages/User"
 fi
 
-# Add taps
-brew tap derailed/k9s
-brew tap heroku/brew
-brew tap Yleisradio/terraforms
+print_header "Initializing brew cache"
+init_brew_cache
 
-# Install packages
-brew install --formula autoenv
-brew install --formula bash
-brew install --formula bat
-brew install --formula blueutil
-brew install --formula ccache
-brew install --formula chtf
-brew install --formula coreutils
-brew install --formula dive
-# brew install --formula dockutil
-brew install --formula fd
-brew install --formula findutils
-brew install --formula fzf
-brew install --formula git
-brew install --formula git-delta
-brew install --formula go
-brew install --formula grep
-brew install --formula hadolint
-brew install --formula helm
-brew install --formula httpie
-brew install --formula jq
-brew install --formula k9s
-brew install --formula kubernetes-cli
-brew install --formula mas
-brew install --formula nano
-brew install --formula openssl # Used for compiling (e.g. pyenv building python versions from source)
-brew install --formula shellcheck
-brew install --formula shfmt
-brew install --formula ssh-copy-id
-brew install --formula stern
-brew install --formula terraform
-brew install --formula tree
-brew install --formula trivy
-brew install --formula unar
-brew install --formula vals
-brew install --formula wget
-brew install --formula wifi-password
-brew install --formula yamllint
-brew install --formula yq
-brew install --formula zlib # Used for compiling (e.g. pyenv building python versions from source)
-brew install --formula zunit-zsh/zunit/zunit
+print_header "Adding taps"
+tap_if_needed "derailed/k9s"
+tap_if_needed "heroku/brew"
+tap_if_needed "yleisradio/terraforms"
+tap_if_needed "zunit-zsh/zunit"
+
+print_header "Installing formulas"
+install_if_needed "autoenv" "formula"
+install_if_needed "bash" "formula"
+install_if_needed "bat" "formula"
+install_if_needed "blueutil" "formula"
+install_if_needed "ccache" "formula"
+install_if_needed "chtf" "formula"
+install_if_needed "coreutils" "formula"
+install_if_needed "dive" "formula"
+install_if_needed "dockutil" "formula"
+install_if_needed "fd" "formula"
+install_if_needed "findutils" "formula"
+install_if_needed "fzf" "formula"
+install_if_needed "git" "formula"
+install_if_needed "git-delta" "formula"
+install_if_needed "go" "formula"
+install_if_needed "grep" "formula"
+install_if_needed "hadolint" "formula"
+install_if_needed "helm" "formula"
+install_if_needed "httpie" "formula"
+install_if_needed "jq" "formula"
+install_if_needed "k9s" "formula"
+install_if_needed "kubernetes-cli" "formula"
+install_if_needed "mas" "formula"
+install_if_needed "nano" "formula"
+install_if_needed "openssl@3" "formula" # Used for compiling (e.g. pyenv building python versions from source)
+install_if_needed "shellcheck" "formula"
+install_if_needed "shfmt" "formula"
+install_if_needed "ssh-copy-id" "formula"
+install_if_needed "stern" "formula"
+install_if_needed "terraform" "formula"
+install_if_needed "tree" "formula"
+install_if_needed "trivy" "formula"
+install_if_needed "unar" "formula"
+install_if_needed "vals" "formula"
+install_if_needed "wget" "formula"
+install_if_needed "wifi-password" "formula"
+install_if_needed "yamllint" "formula"
+install_if_needed "yq" "formula"
+install_if_needed "zlib" "formula" # Used for compiling (e.g. pyenv building python versions from source)
+install_if_needed "zunit" "formula"
 
 # Install cloud cli tools
-brew install --formula awscli
+install_if_needed "awscli" "formula"
 
-# Install applications
-brew install --cask alfred
-brew install --cask backuploupe
-brew install --cask bartender
-brew install --cask bluesnooze
-brew install --cask calibre
-brew install --cask docker
-brew install --cask evernote
-brew install --cask flux
-brew install --cask firefox
-brew install --cask gpg-suite
-brew install --cask hazel
-brew install --cask hpedrorodrigues/tools/dockutil
-brew install --cask iterm2
-brew install --cask keepingyouawake
-brew install --cask monitorcontrol
-brew install --cask multipass
-brew install --cask obsidian
-brew install --cask openlens
-brew install --cask oversight
-brew install --cask postico
-brew install --cask postman
-brew install --cask rectangle
-brew install --cask silicon
-brew install --cask slack
-brew install --cask spotify
-brew install --cask sublime-text
-brew install --cask tableplus
-brew install --cask the-unarchiver
-brew install --cask vagrant
-brew install --cask via
-brew install --cask viscosity
-brew install --cask visual-studio-code
-brew install --cask zoom
+print_header "Installing casks"
+install_if_needed "alfred" "cask"
+install_if_needed "backuploupe" "cask"
+install_if_needed "bartender" "cask"
+install_if_needed "bluesnooze" "cask"
+install_if_needed "calibre" "cask"
+install_if_needed "docker" "cask"
+install_if_needed "evernote" "cask"
+install_if_needed "flux" "cask"
+install_if_needed "firefox" "cask"
+install_if_needed "gpg-suite" "cask"
+install_if_needed "hazel" "cask"
+install_if_needed "iterm2" "cask"
+install_if_needed "keepingyouawake" "cask"
+install_if_needed "monitorcontrol" "cask"
+install_if_needed "multipass" "cask"
+install_if_needed "obsidian" "cask"
+install_if_needed "openlens" "cask"
+install_if_needed "oversight" "cask"
+install_if_needed "postico" "cask"
+install_if_needed "postman" "cask"
+install_if_needed "rectangle" "cask"
+install_if_needed "silicon" "cask"
+install_if_needed "slack" "cask"
+install_if_needed "spotify" "cask"
+install_if_needed "sublime-text" "cask"
+install_if_needed "tableplus" "cask"
+install_if_needed "the-unarchiver" "cask"
+install_if_needed "vagrant" "cask"
+install_if_needed "via" "cask"
+install_if_needed "viscosity" "cask"
+install_if_needed "visual-studio-code" "cask"
+install_if_needed "zoom" "cask"
 
-# Install fonts
-brew install --cask font-fira-code
-brew install --cask font-meslo-lg-nerd-font
-brew install --cask font-fira-code-nerd-font
-brew install --cask font-hack-nerd-font
-brew install --cask font-inconsolata-nerd-font
-brew install --cask font-sauce-code-pro-nerd-font
+print_header "Installing fonts"
+install_if_needed "font-fira-code" "cask"
+install_if_needed "font-meslo-lg-nerd-font" "cask"
+install_if_needed "font-fira-code-nerd-font" "cask"
+install_if_needed "font-hack-nerd-font" "cask"
+install_if_needed "font-inconsolata-nerd-font" "cask"
+install_if_needed "font-sauce-code-pro-nerd-font" "cask"
 
-# TLS/SSL Setup local Certificate Authority and add to the system and firefox trust store
-brew install --formula mkcert
-brew install --formula nss
+print_header "Adding local TLS Certificate Authority"
+install_if_needed "mkcert" "formula"
+install_if_needed "nss" "formula"
 mkcert -install
 
+print_header "Running brew doctor"
+brew doctor
+
 # Add helm charts repository
+print_header "Adding helm charts repositories"
 helm repo add stable https://charts.helm.sh/stable
 
 # Add brew installed bash as an allowed shell
+print_header "Adding brew installed bash as an allowed shell"
 grep "$(which bash)" /etc/shells &>/dev/null || sudo zsh -c "echo $(which bash) >> /etc/shells"
