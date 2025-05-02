9ace20fc8c94f4daab941ed3335e1e318b9edc8e
Austin McConnell <austin.s.mcconnell@gmail.com>
2025-05-02 11:22:50 -0500

    Make RubyGems and Bundle XDG compliant

--- etc/zsh/.zshenv
+++ etc/zsh/.zshenv
@@ -81,6 +81,11 @@ export RBENV_ROOT="$XDG_DATA_HOME/rbenv"
 
 # Ruby
 export IRBRC="$XDG_CONFIG_HOME/irb/irbrc"
+export GEM_HOME="$XDG_DATA_HOME/gem"
+export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem/specs"
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME/bundle/config"
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME/bundle/plugin"
 
 # Less
 export LESSKEY="$XDG_CONFIG_HOME/less/lesskey"
--- install/ruby.sh
+++ install/ruby.sh
@@ -28,12 +28,19 @@ DEFAULT_RUBY_VERSION=3.2.2
 RBENV_ROOT="$XDG_DATA_HOME/rbenv"
 PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
 
+# Create XDG directories for Ruby-related tools
 mkdir -p "$RBENV_ROOT"
 mkdir -p "$RBENV_ROOT/plugins"
 mkdir -p "$XDG_CONFIG_HOME/rubocop"
 mkdir -p "$XDG_CONFIG_HOME/irb"
 mkdir -p "$XDG_DATA_HOME/irb"
+mkdir -p "$XDG_DATA_HOME/gem"
+mkdir -p "$XDG_CACHE_HOME/gem/specs"
+mkdir -p "$XDG_CONFIG_HOME/bundle"
+mkdir -p "$XDG_CACHE_HOME/bundle"
+mkdir -p "$XDG_DATA_HOME/bundle/plugin"
 
+# Migrate IRB history files
 if [[ -f "$HOME/.irb_history" ]]; then
     echo "Moving existing .irb_history file to XDG_DATA_HOME/irb/history"
     mv "$HOME/.irb_history" "$XDG_DATA_HOME/irb/history"
@@ -44,10 +51,41 @@ if [[ -f "$HOME/.irb-history" ]]; then
     mv "$HOME/.irb-history" "$XDG_DATA_HOME/irb/history"
 fi
 
+# Migrate RubyGems files
+if [[ -d "$HOME/.gem" ]]; then
+    echo "Moving existing .gem directory contents to XDG_DATA_HOME/gem"
+    if [[ -d "$HOME/.gem/specs" ]]; then
+        cp -r "$HOME/.gem/specs/"* "$XDG_CACHE_HOME/gem/specs/"
+    fi
+    # Copy any other important gem files/directories
+    if [[ -d "$HOME/.gem/ruby" ]]; then
+        mkdir -p "$XDG_DATA_HOME/gem/ruby"
+        cp -r "$HOME/.gem/ruby/"* "$XDG_DATA_HOME/gem/ruby/"
+    fi
+    rm -rf "$HOME/.gem"
+fi
+
+# Migrate Bundler files
+if [[ -d "$HOME/.bundle" ]]; then
+    echo "Moving existing .bundle directory contents to XDG locations"
+    if [[ -f "$HOME/.bundle/config" ]]; then
+        cp "$HOME/.bundle/config" "$XDG_CONFIG_HOME/bundle/config"
+    fi
+    if [[ -d "$HOME/.bundle/cache" ]]; then
+        cp -r "$HOME/.bundle/cache/"* "$XDG_CACHE_HOME/bundle/"
+    fi
+    if [[ -d "$HOME/.bundle/plugin" ]]; then
+        cp -r "$HOME/.bundle/plugin/"* "$XDG_DATA_HOME/bundle/plugin/"
+    fi
+    rm -rf "$HOME/.bundle"
+fi
+
+# Link configuration files
 ln -sfv "$DOTFILES_DIR/etc/ruby/default-gems" "$RBENV_ROOT/default-gems"
 ln -sfv "$DOTFILES_DIR/etc/ruby/rubocop.yml" "$XDG_CONFIG_HOME/rubocop/config.yml"
 ln -sfv "$DOTFILES_DIR/etc/ruby/irbrc" "$XDG_CONFIG_HOME/irb/irbrc"
 
+# Install rbenv plugins
 REPO_DIR="$RBENV_ROOT/plugins/rbenv-default-gems"
 if [ -d "$REPO_DIR/.git" ]; then
     git --work-tree="$REPO_DIR" --git-dir="$REPO_DIR/.git" pull origin master
@@ -62,6 +100,7 @@ else
     git clone https://github.com/rkh/rbenv-update.git "$REPO_DIR"
 fi
 
+# Install Ruby
 rbenv install --skip-existing "${DEFAULT_RUBY_VERSION}"
 rbenv global "${DEFAULT_RUBY_VERSION}"
 
