40d2790470523c09451bbc52a6acf55ff0f0dc2c
Austin McConnell <austin.s.mcconnell@gmail.com>
2025-05-04 10:45:43 -0500

    Implement static loading for Antidote plugins

--- etc/zsh/zshrc.d/10-antidote.zsh
+++ etc/zsh/zshrc.d/10-antidote.zsh
@@ -3,5 +3,14 @@
 # 10-antidote.zsh - Load and configure antidote plugin manager
 #
 
+# Load antidote
 source ${ZDOTDIR:-~}/.antidote/antidote.zsh
-antidote load
+
+# Generate static plugin file if needed
+zsh_plugins=${ZDOTDIR:-~}/.zsh_plugins
+if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+  antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
+fi
+
+# Source the static plugins file
+source ${zsh_plugins}.zsh
