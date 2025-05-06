c532e6be8f16f363ec7b6d4136389336430090fa
Austin McConnell <austin.s.mcconnell@gmail.com>
2025-05-06 15:47:36 -0500

    Remove leftover zshrc.d config

--- etc/zsh/zshrc.d/00-fpath.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-#
-# 00-fpath.zsh - Configure function paths and autoload functions
-#
-
-# Add functions directory to fpath and autoload all functions
-fpath=($DOTFILES_DIR/etc/zsh/functions $fpath)
-autoload -Uz $DOTFILES_DIR/etc/zsh/functions/*
--- etc/zsh/zshrc.d/99-profiling.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-#
-# 99-profiling.zsh - End profiling if enabled
-#
-
-# done profiling
-[[ ${ZSH_PROFILE_RC:-0} -eq 0 ]] || { unset ZSH_PROFILE_RC && zprof }
