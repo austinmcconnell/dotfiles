1f3fcaf76e7a7bb7e54cc71afb58dd577bd97ec8
Austin McConnell <austin.s.mcconnell@gmail.com>
2026-04-30 09:41:19 -0500

    fix(install): add cross-platform support to brew and zsh
    brew.sh was macOS-only — running it on Linux returned early
    with "Not macOS" and skipped all formula installations. Casks,
    fonts, mkcert, and brew doctor ran unconditionally and failed
    on Linux. Sublime-text symlinks were hardcoded to the macOS
    Library path. Starship was not installed on Linux at all.
    
    - Add Linux Homebrew installation in brew.sh
    - Wrap casks, fonts, mkcert, and brew doctor in is-macos guard
    - Move blueutil, dockutil, mas, wifi-password to macOS section
    - Refactor sublime-text symlinks with OS-detected config path
    - Add missing molovo/revolver tap (needed by zunit)
    - Install starship via brew on Linux in zsh.sh

--- install/brew.sh
+++ install/brew.sh
@@ -11,8 +11,13 @@ else
     if is-macos; then
         print_header "Installing Homebrew"
         NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    elif is-debian; then
+        print_header "Installing Homebrew on Linux"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        # Add Homebrew to PATH for current session
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     else
-        print_header "Skipping Homebrew installation: Not macOS"
+        print_header "Skipping Homebrew installation: Unsupported OS"
         return
     fi
 fi
@@ -26,15 +31,27 @@ ln -sfv "$DOTFILES_DIR/etc/misc/shellcheckrc" "$XDG_CONFIG_HOME/shellcheckrc"
 ln -sfv "$DOTFILES_DIR/etc/fd/ignore" "$XDG_CONFIG_HOME/fd/ignore"
 ln -sfv "$DOTFILES_DIR/etc/httpie/config.json" "$XDG_CONFIG_HOME/httpie/config.json"
 ln -sfv "$DOTFILES_DIR/etc/bat/config" "$XDG_CONFIG_HOME/bat/config"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/JsPrettier.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/Fmt.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/Python.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/SublimeLinter.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User"
-ln -sfv "$DOTFILES_DIR/etc/sublime-text/lsp_library_installer.py" "$HOME/Library/Application Support/Sublime Text/Packages/User"
+
+# Sublime Text configuration
 if is-macos; then
-    ln -sfv "$DOTFILES_DIR/etc/sublime-text/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text/Packages/User"
+    SUBLIME_USER_DIR="$HOME/Library/Application Support/Sublime Text/Packages/User"
+elif is-debian; then
+    SUBLIME_USER_DIR="$XDG_CONFIG_HOME/sublime-text/Packages/User"
+fi
+
+if [ -n "$SUBLIME_USER_DIR" ]; then
+    mkdir -p "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/Fmt.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/JsPrettier.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/Package Control.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/Preferences.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/Python.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/SublimeLinter.sublime-settings" "$SUBLIME_USER_DIR"
+    ln -sfv "$DOTFILES_DIR/etc/sublime-text/lsp_library_installer.py" "$SUBLIME_USER_DIR"
+
+    if is-macos; then
+        ln -sfv "$DOTFILES_DIR/etc/sublime-text/Default (OSX).sublime-keymap" "$SUBLIME_USER_DIR"
+    fi
 fi
 
 init_brew_cache
@@ -42,18 +59,17 @@ init_brew_cache
 print_header "Adding taps"
 tap_if_needed "derailed/k9s"
 tap_if_needed "heroku/brew"
+tap_if_needed "molovo/revolver"
 tap_if_needed "zunit-zsh/zunit"
 
 print_header "Installing formulas"
 install_if_needed "autoenv" "formula"
 install_if_needed "bash" "formula"
 install_if_needed "bat" "formula"
-install_if_needed "blueutil" "formula"
 install_if_needed "ccache" "formula"
 install_if_needed "coreutils" "formula"
 install_if_needed "direnv" "formula"
 install_if_needed "dive" "formula"
-install_if_needed "dockutil" "formula"
 install_if_needed "fd" "formula"
 install_if_needed "findutils" "formula"
 install_if_needed "fzf" "formula"
@@ -69,7 +85,6 @@ install_if_needed "httpie" "formula"
 install_if_needed "jq" "formula"
 install_if_needed "k9s" "formula"
 install_if_needed "kubernetes-cli" "formula"
-install_if_needed "mas" "formula" "personal"
 install_if_needed "nano" "formula"
 install_if_needed "openssl@3" "formula" # Used for compiling (e.g. pyenv building python versions from source)
 install_if_needed "shellcheck" "formula"
@@ -82,7 +97,6 @@ install_if_needed "trivy" "formula"
 install_if_needed "unar" "formula"
 install_if_needed "vals" "formula"
 install_if_needed "wget" "formula"
-install_if_needed "wifi-password" "formula"
 install_if_needed "yamllint" "formula"
 install_if_needed "yq" "formula"
 install_if_needed "zlib" "formula" # Used for compiling (e.g. pyenv building python versions from source)
@@ -91,58 +105,68 @@ install_if_needed "zunit" "formula"
 # Install cloud cli tools
 install_if_needed "awscli" "formula"
 
-print_header "Installing casks"
-install_if_needed "alfred" "cask"
-install_if_needed "backuploupe" "cask" "personal"
-install_if_needed "bartender" "cask"
-install_if_needed "bluesnooze" "cask"
-install_if_needed "calibre" "cask" "personal"
-install_if_needed "chatgpt" "cask"
-install_if_needed "discord" "cask" "personal"
-install_if_needed "docker" "cask" "personal"
-install_if_needed "evernote" "cask"
-install_if_needed "flux" "cask"
-install_if_needed "firefox" "cask"
-install_if_needed "gpg-suite" "cask"
-install_if_needed "hazel" "cask" "personal"
-install_if_needed "iterm2" "cask"
-install_if_needed "keepingyouawake" "cask" "personal"
-install_if_needed "monitorcontrol" "cask"
-install_if_needed "multipass" "cask"
-install_if_needed "obsidian" "cask"
-install_if_needed "openlens" "cask"
-install_if_needed "oversight" "cask"
-install_if_needed "postico" "cask"
-install_if_needed "postman" "cask"
-install_if_needed "rectangle" "cask"
-install_if_needed "silicon" "cask"
-install_if_needed "slack" "cask" "personal"
-install_if_needed "spotify" "cask"
-install_if_needed "sublime-text" "cask"
-install_if_needed "tableplus" "cask"
-install_if_needed "the-unarchiver" "cask"
-install_if_needed "typora" "cask"
-install_if_needed "vagrant" "cask"
-install_if_needed "via" "cask"
-install_if_needed "viscosity" "cask" "personal"
-install_if_needed "visual-studio-code" "cask"
-install_if_needed "zoom" "cask"
+if is-macos; then
+    print_header "Installing macOS-only formulas"
+    install_if_needed "blueutil" "formula"
+    install_if_needed "dockutil" "formula"
+    install_if_needed "mas" "formula" "personal"
+    install_if_needed "wifi-password" "formula"
+fi
 
-print_header "Installing fonts"
-install_if_needed "font-fira-code" "cask"
-install_if_needed "font-meslo-lg-nerd-font" "cask"
-install_if_needed "font-fira-code-nerd-font" "cask"
-install_if_needed "font-hack-nerd-font" "cask"
-install_if_needed "font-inconsolata-nerd-font" "cask"
-install_if_needed "font-sauce-code-pro-nerd-font" "cask"
+if is-macos; then
+    print_header "Installing casks"
+    install_if_needed "alfred" "cask"
+    install_if_needed "backuploupe" "cask" "personal"
+    install_if_needed "bartender" "cask"
+    install_if_needed "bluesnooze" "cask"
+    install_if_needed "calibre" "cask" "personal"
+    install_if_needed "chatgpt" "cask"
+    install_if_needed "discord" "cask" "personal"
+    install_if_needed "docker" "cask" "personal"
+    install_if_needed "evernote" "cask"
+    install_if_needed "flux" "cask"
+    install_if_needed "firefox" "cask"
+    install_if_needed "gpg-suite" "cask"
+    install_if_needed "hazel" "cask" "personal"
+    install_if_needed "iterm2" "cask"
+    install_if_needed "keepingyouawake" "cask" "personal"
+    install_if_needed "monitorcontrol" "cask"
+    install_if_needed "multipass" "cask"
+    install_if_needed "obsidian" "cask"
+    install_if_needed "openlens" "cask"
+    install_if_needed "oversight" "cask"
+    install_if_needed "postico" "cask"
+    install_if_needed "postman" "cask"
+    install_if_needed "rectangle" "cask"
+    install_if_needed "silicon" "cask"
+    install_if_needed "slack" "cask" "personal"
+    install_if_needed "spotify" "cask"
+    install_if_needed "sublime-text" "cask"
+    install_if_needed "tableplus" "cask"
+    install_if_needed "the-unarchiver" "cask"
+    install_if_needed "typora" "cask"
+    install_if_needed "vagrant" "cask"
+    install_if_needed "via" "cask"
+    install_if_needed "viscosity" "cask" "personal"
+    install_if_needed "visual-studio-code" "cask"
+    install_if_needed "zoom" "cask"
 
-print_header "Adding local TLS Certificate Authority"
-install_if_needed "mkcert" "formula"
-install_if_needed "nss" "formula"
-mkcert -install
+    print_header "Installing fonts"
+    install_if_needed "font-fira-code" "cask"
+    install_if_needed "font-meslo-lg-nerd-font" "cask"
+    install_if_needed "font-fira-code-nerd-font" "cask"
+    install_if_needed "font-hack-nerd-font" "cask"
+    install_if_needed "font-inconsolata-nerd-font" "cask"
+    install_if_needed "font-sauce-code-pro-nerd-font" "cask"
 
-print_header "Running brew doctor"
-brew doctor || true
+    print_header "Adding local TLS Certificate Authority"
+    install_if_needed "mkcert" "formula"
+    install_if_needed "nss" "formula"
+    mkcert -install
+
+    print_header "Running brew doctor"
+    brew doctor || true
+fi
 
 # Add helm charts repository
 echo "Adding helm charts repositories"
