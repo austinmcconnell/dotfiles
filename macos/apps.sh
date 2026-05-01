bcf127ac2dec010725eb80b245aa5f71b4cb5c39
Austin McConnell <austin.s.mcconnell@gmail.com>
2026-05-01 14:29:30 -0500

    chore(macos): drop mac app store apps that no longer exist

--- macos/apps.sh
+++ macos/apps.sh
@@ -18,7 +18,6 @@ fi
 
 # Mac App Store application
 mas install 1236045954 # Canary mail
-mas install 926036361  # Lastpass
 mas install 1289583905 # Pixelmator Pro
 mas install 1303222628 # Paprika Recipe Manager 3
 mas install 714196447  # Menubar Stats
@@ -26,4 +25,3 @@ mas install 1448031908 # Weather for Status Bar
 
 # Safari Extensions
 mas install 1481669779 # Evernote Web Clipper
-mas install 957810159  # Raindrop.io for Safari
