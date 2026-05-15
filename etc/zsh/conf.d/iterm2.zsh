199f080d298b9ccaf581c810785145d25b1cb386
Austin McConnell <austin.s.mcconnell@gmail.com>
2026-05-15 14:12:01 -0500

    feat(iterm2): set window title to git repo or directory name
    Windows are hard to distinguish in Mission Control when multiple
    iTerm2 instances are open. Use a precmd hook to dynamically set
    the tab/window title to the current git repo basename, falling
    back to the directory basename.
    
    - Add precmd hook using OSC 1 escape sequence
    - Set Title Components to Session Name only in profile plist

--- etc/zsh/conf.d/iterm2.zsh
+++ etc/zsh/conf.d/iterm2.zsh
@@ -5,5 +5,13 @@
 
 # Load iTerm2 shell integration if it exists
 if [[ -f "${HOME}/.iterm2_shell_integration.zsh" ]]; then
-  source "${HOME}/.iterm2_shell_integration.zsh"
+    source "${HOME}/.iterm2_shell_integration.zsh"
 fi
+
+# Set tab/window title to git repo basename or directory basename
+function precmd_iterm2_title() {
+    local dir
+    dir=$(git rev-parse --show-toplevel 2>/dev/null || pwd)
+    echo -ne "\e]1;${dir:t}\a"
+}
+add-zsh-hook precmd precmd_iterm2_title
